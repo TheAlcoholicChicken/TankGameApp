d30df264fc7da8951dc20e1fc00eff3d02658c75
PashanIrani <pashanirani@gmail.com>
2018-11-14 21:19:09 -0800

    commit message lol

--- controllers/controller_db.js
+++ controllers/controller_db.js
@@ -3,7 +3,9 @@
  */
 const mongoose = require('mongoose');
 const uriString = process.env.DB_URL || 'mongodb://localhost:27017';
-const dbName = 'TankGame';
+const dbName = process.env.DB_NAME || 'TankGame';
+const url = uriString + '/' + dbName;
+console.log(url);
 
 let User = new mongoose.Schema({
     user_name: String,
@@ -12,7 +14,7 @@ let User = new mongoose.Schema({
 });
 
 mongoose.connect(
-    uriString + '/' + dbName,
+    url,
     { useNewUrlParser: true }
 );
 
